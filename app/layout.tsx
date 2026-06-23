fa351bd14ddf1df0f0a4ae284e64cad603d046d1
Yuri <eugenewoolf220205@gmail.com>
2026-06-23 18:09:35 +0530

    github change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import { Geist_Mono } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
 import { SidebarWrapper } from "@/components/SidebarWrapper";
 import { LenisProvider } from "@/components/LenisProvider";
 import "lenis/dist/lenis.css";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,7 +35,7 @@ export default function RootLayout({
     <html
       lang="en"
       data-scroll-behavior="smooth"
-      className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", inter.variable, geistMono.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full bg-[#0a0a0a] text-[#f9fafb]" suppressHydrationWarning>
         <LenisProvider>
